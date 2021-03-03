2857d56e733b6612262f163b4b436394d7c58326
phuong <phuonghqh@gmail.com>
2021-03-03 19:33:47 +0700

    Update WF

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#pragma comment(lib, "crypt32.lib")
+
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <Wincrypt.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main (void) {
+    std::cout << "Hello World!";
+    return 0;
+}
